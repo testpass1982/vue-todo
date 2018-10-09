2ff8c2aa0789e1effad1f443090fdccad77f1e34
Anatoliy Popov <popov@naks.ru>
2018-10-09 11:35:48 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <h4 class="bg-primary text-white text-center p-2"> {{ name }}'s To Do List</h4>
+    <div class="container-fluid p-4">
+     <div class="row">
+       <div class="col font-weight-bold">Task</div>
+       <div class="col-2 font-weight-bold">Done</div>
+     </div>
+     <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">    
+        <div class="col">{{ t.action }}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input"/>
+          {{ t.done }}
+          </div>
+      </div>
+
+    <div class="row py-2">
+      <div class="col">
+        <input v-model="newItemText" class="form-control"/>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+      </div>
+    </div>
+
+    <div class="row bg-secondary py-2 mt-2 text-white">
+      <div class="col text-center">
+        <input type="checkbox" v-model="hideCompleted" class="form-check-input"/>
+        <label class="form-check-label font-weight-bold">
+          Hide completed tasks
+        </label>
+      </div>
+    </div>
+  </div> <!--container-fluid-->
+  </div> <!--end-app-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+      data() {
+        return {
+          name: "Adam",
+          tasks: [{ action: "Buy Flowers", done: false},
+                  { action: "Get Shoes", done: false},
+                  { action: "Collect Tickets", done: true},
+                  { action: "Call Joe", done: false}],
+          hideCompleted: true,
+          newItemText: ""
+        }
+      },
+      computed: {
+        filteredTasks() {
+          return this.hideCompleted ?
+            this.tasks.filter(t => !t.done) : this.tasks
+        }
+      },
+      methods: {
+        addNewTodo() {
+          this.tasks.push({
+            action: this.newItemText,
+            done: false
+          });
+          this.newItemText="";
+        }
+      }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
